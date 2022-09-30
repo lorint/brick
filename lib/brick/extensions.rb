aa3d7be3eba50000a39818fe7fc40532d6443ebe
Lorin Thwaits <lorint@gmail.com>
2022-09-30 11:38:19 +0100

    Cause SQLite to start transactions in IMMEDIATE mode

--- lib/brick/extensions.rb
+++ lib/brick/extensions.rb
@@ -1434,6 +1434,27 @@ module ActiveRecord::ConnectionHandling
   alias _brick_establish_connection establish_connection
   def establish_connection(*args)
     conn = _brick_establish_connection(*args)
+      # Overwrite SQLite's #begin_db_transaction so it opens in IMMEDIATE mode instead of
+      # the default DEFERRED mode.
+      #   https://discuss.rubyonrails.org/t/failed-write-transaction-upgrades-in-sqlite3/81480/2
+      if ActiveRecord::Base.connection.adapter_name == 'SQLite'
+        arca = ::ActiveRecord::ConnectionAdapters
+        db_statements = arca::SQLite3::DatabaseStatements
+        # Rails 7.1 and later
+        if arca::AbstractAdapter.private_instance_methods.include?(:with_raw_connection)
+          db_statements.define_method(:begin_db_transaction) do
+            log("begin immediate transaction", "TRANSACTION") do
+              with_raw_connection(allow_retry: true, uses_transaction: false) do |conn|
+                conn.transaction(:immediate)
+              end
+            end
+          end
+        else # Rails < 7.1
+          db_statements.define_method(:begin_db_transaction) do
+            log('begin immediate transaction', 'TRANSACTION') { @connection.transaction(:immediate) }
+          end
+        end
+      end
     begin
       _brick_reflect_tables
     rescue ActiveRecord::NoDatabaseError
