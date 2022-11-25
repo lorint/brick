861915ca0bf221dabb54fe3dbc135af09b276f36
Lorin Thwaits <lorint@gmail.com>
2022-11-25 08:47:42 +0000

    Make .eager_load() patch active only when .select(...) is present

--- lib/brick.rb
+++ lib/brick.rb
@@ -1284,38 +1284,38 @@ module ActiveRecord
             else
               relation.select_values.clear
             end
+
+            @aliases ||= Aliases.new(join_root.each_with_index.map do |join_part, i|
+              join_alias = join_part.table&.table_alias || join_part.table_name
+              keys = [join_part.base_klass.primary_key] # Always include the primary key
+
+              # # %%% Optional to include all foreign keys:
+              # keys.concat(join_part.base_klass.reflect_on_all_associations.select { |a| a.belongs_to? }.map(&:foreign_key))
+
+              # Add foreign keys out to referenced tables that we belongs_to
+              join_part.children.each { |child| keys << child.reflection.foreign_key if child.reflection.belongs_to? }
+
+              # Add the foreign key that got us here -- "the train we rode in on" -- if we arrived from
+              # a has_many or has_one:
+              if join_part.is_a?(ActiveRecord::Associations::JoinDependency::JoinAssociation) &&
+                 !join_part.reflection.belongs_to?
+                keys << join_part.reflection.foreign_key
+              end
+              keys = keys.compact # In case we're using composite_primary_keys
+              j = 0
+              columns = join_part.column_names.each_with_object([]) do |column_name, s|
+                # Include columns chosen in select(...) as well as the PK and any relevant FKs
+                if used_cols.keys.find { |c| (c.length == 1 || c.first == join_alias) && c.last == column_name } ||
+                   keys.find { |c| c == column_name }
+                  s << Aliases::Column.new(column_name, "t#{i}_r#{j}")
+                end
+                j += 1
+              end
+              Aliases::Table.new(join_part, columns)
+            end)
           end
 
-          @aliases ||= Aliases.new(join_root.each_with_index.map do |join_part, i|
-            join_alias = join_part.table&.table_alias || join_part.table_name
-            keys = [join_part.base_klass.primary_key] # Always include the primary key
-
-            # # %%% Optional to include all foreign keys:
-            # keys.concat(join_part.base_klass.reflect_on_all_associations.select { |a| a.belongs_to? }.map(&:foreign_key))
-
-            # Add foreign keys out to referenced tables that we belongs_to
-            join_part.children.each { |child| keys << child.reflection.foreign_key if child.reflection.belongs_to? }
-
-            # Add the foreign key that got us here -- "the train we rode in on" -- if we arrived from
-            # a has_many or has_one:
-            if join_part.is_a?(ActiveRecord::Associations::JoinDependency::JoinAssociation) &&
-               !join_part.reflection.belongs_to?
-              keys << join_part.reflection.foreign_key
-            end
-            keys = keys.compact # In case we're using composite_primary_keys
-            j = 0
-            columns = join_part.column_names.each_with_object([]) do |column_name, s|
-              # Include columns chosen in select(...) as well as the PK and any relevant FKs
-              if used_cols.keys.find { |c| (c.length == 1 || c.first == join_alias) && c.last == column_name } ||
-                 keys.find { |c| c == column_name }
-                s << Aliases::Column.new(column_name, "t#{i}_r#{j}")
-              end
-              j += 1
-            end
-            Aliases::Table.new(join_part, columns)
-          end)
-
-          relation._select!(-> { @aliases.columns })
+          relation._select!(-> { aliases.columns })
         end
 
       private
