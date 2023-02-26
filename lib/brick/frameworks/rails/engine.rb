dba84370ee79b4019f52fea5c930fec7792199d9
Lorin Thwaits <lorint@gmail.com>
2023-02-26 23:51:06 +0000

    Automatically build out ActiveAdmin dashboard when it's missing

--- lib/brick/frameworks/rails/engine.rb
+++ lib/brick/frameworks/rails/engine.rb
@@ -350,6 +350,21 @@ window.addEventListener(\"popstate\", linkSchemas);
               end
             end
           end
+          # Build out the main dashboard with default boilerplate if it's missing
+          if (namespace = ::ActiveAdmin.application.namespaces.names.first&.to_s) &&
+             !Object.const_defined?("#{namespace.camelize}::Dashboard")
+            ::ActiveAdmin.register_page "Dashboard" do
+              menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+              content title: proc { I18n.t("active_admin.dashboard") } do
+                div class: "blank_slate_container", id: "dashboard_default_message" do
+                  span class: "blank_slate" do
+                    span I18n.t("active_admin.dashboard_welcome.welcome")
+                    small I18n.t("active_admin.dashboard_welcome.call_to_action")
+                  end
+                end
+              end
+            end
+          end
         end
 
         # Forest Admin compatibility
