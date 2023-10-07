dfc3c348f1fe076d36d4c982f37f8b72781dcfe7
Lorin Thwaits <lorin.thwaits@sohohouse.com>
2023-10-07 14:00:54 +0200

    Support Rails 7.1.1

--- Gemfile
+++ Gemfile
@@ -5,5 +5,10 @@ ruby '2.7.8'
 source 'https://rubygems.org'
 gemspec
 
-# gem "rails-controller-testing", "~> 1.0.2"
+# gem 'rails-controller-testing', '~> 1.0.2'
 # gem 'nokogiri', '~> 1.10.10'
+gem 'rswag-ui', '~> 2.11.0'
+
+if Gem::Specification.all.find{|gs| gs.name == 'activerecord'}.version >= Gem::Version.new('7.1.0')
+  require 'active_record/deprecator'
+end
--- spec/test_app/config/routes.rb
+++ spec/test_app/config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-TestApp::Application.routes.draw do
-  # resources :articles, only: [:create]
-  # resources :widgets, only: %i[create update destroy]
-  mount Rswag::Ui::Engine => '/api-docs'
-end
+if ActiveRecord.version > ::Gem::Version.new('4.2')
+  TestApp::Application.routes.draw do
+    # resources :articles, only: [:create]
+    # resources :widgets, only: %i[create update destroy]
+    mount Rswag::Ui::Engine => '/api-docs'
+  end
 
-# Set up documentation endpoint
-Rswag::Ui.configure do |config|
-  config.swagger_endpoint '/api-docs/v1/swagger.json', 'API V1 Docs'
+  # Set up documentation endpoint
+  Rswag::Ui.configure do |config|
+    config.swagger_endpoint '/api-docs/v1/swagger.json', 'API V1 Docs'
+  end
 end
